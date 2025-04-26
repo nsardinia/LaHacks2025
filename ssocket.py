8a9a6051ebfe5d98d4a858ddbb402deb737f9e8b
Ben Sirivallop <bsirival@uci.edu>
2025-04-26 04:20:18 -0700

    this should process something every 20 seconds

--- ssocket.py
+++ ssocket.py
@@ -8,9 +8,10 @@ import numpy as np
 from main import gemini
 from key import TOKEN
 
-async def video_receiver(websocket):
+async def video_receiver(websocket, gemini_):
     print("Client connected")
     try:
+        frame_counter = 0
         while True:
             data = await websocket.recv()
             # Data is expected to be binary (a JPEG image)
@@ -18,8 +19,10 @@ async def video_receiver(websocket):
             frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
             
             if frame is not None:
-                # process(frame)
-                print("RECEIVED a frame")
+                frame_counter+=1
+                if frame_counter >= 600: #this captures a frame every 20 seconds.
+                    gemini_.process(frame)
+                    frame_counter = 0
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
     except websockets.exceptions.ConnectionClosed:
@@ -27,17 +30,19 @@ async def video_receiver(websocket):
     finally:
         cv2.destroyAllWindows()
 
-# def run():
-#     gemin.process()
 
-async def main():
+async def main(gemin):
     #FILL IN THIS IP ADDRESS
-    async with websockets.serve(video_receiver, "0.0.0.0", 8765):
+
+    async def handler(websocket):
+        await video_receiver(websocket, gemin) # Pass the instance
+
+    async with websockets.serve(handler, "0.0.0.0", 8765):
         print("WebSocket server started")
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
     gemin = gemini(TOKEN)
-    asyncio.run(main(),)
+    asyncio.run(main(gemin))
 
 
